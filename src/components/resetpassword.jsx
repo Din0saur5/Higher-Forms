d2119c2b64726fa6cc6fdd6050e600a63be4f0ec
Dan <danhackworth@icloud.com>
2025-03-03 14:28:40 -0500

    fix 2 for rerenders

--- src/components/resetpassword.jsx
+++ src/components/resetpassword.jsx
@@ -38,7 +38,7 @@ function ResetPassword() {
     };
 
     authenticateUser();
-  }, [location.search]);
+  }, []);
 
   const handlePasswordChange = async (e) => {
     e.preventDefault();
@@ -74,7 +74,7 @@ function ResetPassword() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="card border rounded-xl p-4 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form onSubmit={handlePasswordChange} className="card-body">
+        <form onSubmit={(e)=>handlePasswordChange(e)} className="card-body">
           <h3 className="text-center text-xl mb-4">Reset Your Password</h3>
 
           {errorMessage && <div className="text-red-500 text-sm mb-4">{errorMessage}</div>}
